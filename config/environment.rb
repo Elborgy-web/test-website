80c9e1fa825aa557372daecc41da4c7798dd3f5b
Elborgy-Websites <Muhammad.elborgy@gmail.com>
2017-05-31 10:01:24 +0000

    Added devise gem for users

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
 ActionMailer::Base.smtp_settings = {
-    :address => 'smtp_sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID__USERNAME'],
-    :password => ENV['SENDGRID__PASSWORD'],
-    :domain => 'heroku.com',
-    :enable_startstls_auto => true
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_startstls_auto => true
 }
