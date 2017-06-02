ff286a2510b479e1246ab7b0b0bc9df3adbc639b
Elborgy-Websites <Muhammad.elborgy@gmail.com>
2017-06-02 12:34:55 +0000

    Modified devise for stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  before_filter :configure_premitted_paramaters, if: devise_controller?
+  before_filter :configure_premitted_paramaters, if: :devise_controller?
   
   protected
     def configure_premitted_paramaters
