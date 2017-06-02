63f2dbc49aece4df2b450532071af7957eb985af
Elborgy-Websites <Muhammad.elborgy@gmail.com>
2017-06-02 14:50:18 +0000

    Added profile new acction

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,6 @@
 class ProfilesController < ApplicationController
-    
+    def new
+        # form where a user can fillout their own profile.
+        @user = User.find( params[:user_id] )
+    end
 end
